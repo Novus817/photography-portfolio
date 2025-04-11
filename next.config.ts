409187b2a3f9430ab23ca9a8e14b9fc46bd818df
Anthony Marrello <apm817@gmail.com>
2025-04-11 01:14:39 -0400

    extend config with cloudinary

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  env: {
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  },
 };
 
 export default nextConfig;
