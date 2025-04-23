33320263b8e00285c0966e777eefdb08c223dbf0
Anthony Marrello <apm817@gmail.com>
2025-04-23 00:50:58 -0400

    add contact component

---
+++ app/contact/page.tsx
@@ -0,0 +1,16 @@
+export default function Contact() {
+  return (
+    <div className="container mx-auto w-135 px-4 py-8">
+      <h1 className="text-3xl font-bold mb-4">Get in Touch</h1>
+      <p className="text-lg">
+        Reach me at&nbsp;
+        <a
+          href="mailto:dev@testing.com"
+          className="underline hover:text-gray-300"
+        >
+          dev@testing.com
+        </a>
+      </p>
+    </div>
+  );
+}
