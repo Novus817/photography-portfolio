e2aee28dc28f40eea21541099a2b8dab79cb603f
Anthony Marrello <apm817@gmail.com>
2025-04-23 00:50:47 -0400

    add about component

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div className="container mx-auto w-135 px-4 py-8">
+      <h1 className="text-3xl font-bold mb-4">About Me</h1>
+      <p className="text-lg">
+        One of my main hobbies is photography and am very passionate about being
+        out in nature and trying to capture different moments. My work focuses on
+        landscapes mainly but I enjoy every day life or whatever catches my eye.
+      </p>
+    </div>
+  );
+}
